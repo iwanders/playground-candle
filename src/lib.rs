d7f8075605eb0653b596ee47c79945fc56dacd89
Ivor Wanders <ivor@iwanders.net>
2024-06-22 13:35:03 -0400

    Loading the dataset.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
 
 pub type MainResult = anyhow::Result<()>;
 pub fn main() -> MainResult {
+    let args = std::env::args().collect::<Vec<String>>();
+    let m = candle_datasets::vision::mnist::load_dir(&args[1])?;
+
+
+    println!("train-images: {:?}", m.train_images.shape());
+    println!("train-labels: {:?}", m.train_labels.shape());
+    println!("test-images: {:?}", m.test_images.shape());
+    println!("test-labels: {:?}", m.test_labels.shape());
+
     Ok(())
 }
